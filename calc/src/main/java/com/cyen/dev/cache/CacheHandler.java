100daba02bc4f4cd6463915d0924e23c303c1b22
Cyen <qingshanpeng@126.com>
2019-05-23 15:21:08 +0800

    添加括号运算

--- calc/src/main/java/com/cyen/dev/cache/CacheHandler.java
+++ calc/src/main/java/com/cyen/dev/cache/CacheHandler.java
@@ -1,19 +1,25 @@
 package com.cyen.dev.cache;
 
 import com.cyen.dev.CalcHandler;
-import com.cyen.dev.OperationFactory;
 import com.cyen.dev.OperationType;
 
 /**
  * 进行解析处理
+ * 例如：4*3/6+(6-4)*2-7、(6*1+2*3-1+5*2-1)-9
  *
  * @author qingsp
  * @date: 2019-05-16
  */
-public class CacheHandler implements CalcHandler {
+public class CacheHandler extends CalcSupport implements CalcHandler {
 
     private CalcCache calcCache = new CalcCache();
 
+    /**
+     * 计算表达式值
+     *
+     * @param st 表达式
+     * @return 计算结果
+     */
     public double calc(String st) {
         //最后如果为优先级高的符号
         //加上0可以将末尾的优先级高的符号进行计算
@@ -23,16 +29,19 @@ public class CacheHandler implements CalcHandler {
             char s = st.charAt(i);
             if (OperationType.isAdd(s)
                     || OperationType.isSub(s)) {
-                if (calcCache.checkPriority(0)) {
-                    //如果出现优先级低的符号
-                    //计算之前符号优先级高的值
-                    calc(calcCache);
-                }
-                calcCache.setPriority(0);
+                //处理优先级的值
+                handlePriority();
                 calcCache.pushSign(s);
             } else if (OperationType.isMul(s)
                     || OperationType.isDiv(s)) {
                 calcCache.setPriority(1);
+                calcCache.pushSign(s);
+            } else if (OperationType.isRightParenthesis(s)) {
+                //处理优先级的值
+                handlePriority();
+                //计算括号里面的值
+                calcParenthesis(calcCache);
+            } else if (OperationType.isLeftParenthesis(s)) {
                 calcCache.pushSign(s);
             } else {
                 calcCache.pushValue(Double.valueOf(String.valueOf(s)));
@@ -43,84 +52,14 @@ public class CacheHandler implements CalcHandler {
     }
 
     /**
-     * 计算优先级高的值
+     * 处理优先级高的值
      */
-    private void calc(CalcCache calcCache) {
-        CalcCache c = new CalcCache();
-
-        double v2 = calcCache.popValue();
-        char o = calcCache.popSign();
-        double v1 = calcCache.popValue();
-        c.pushValue(v2);
-        c.pushSign(o);
-        c.pushValue(v1);
-
-        char ch = 0;
-        while (!calcCache.signsEmpty()
-                && (OperationType.isMul(ch = calcCache.popSign()) || OperationType.isDiv(ch))) {
-            c.pushSign(ch);
-            double v3 = calcCache.popValue();
-            c.pushValue(v3);
-        }
-        if (!calcCache.signsEmpty() && ch != 0) {
-            calcCache.pushSign(ch);
-        }
-        calcCache.pushValue(calcTempResult(c));
-    }
-
-    /**
-     * 计算临时结果
-     *
-     * @return
-     */
-    private double calcTempResult(CalcCache calcCache) {
-        //打印每次的信息
-        System.out.println(calcCache.toString());
-        //获取栈大小
-        int size = calcCache.valuesSize();
-        if (size == 1) {
-            return calcCache.popValue();
-        } else if (size == 2) {
-            double v1 = calcCache.popValue();
-            char o = calcCache.popSign();
-            double v2 = calcCache.popValue();
-            return OperationFactory.createOperation(o).calc(v1, v2);
-        } else {
-            double v1 = calcCache.popValue();
-            char o = calcCache.popSign();
-            double v2 = calcCache.popValue();
-            double v = OperationFactory.createOperation(o).calc(v1, v2);
-            calcCache.pushValue(v);
-            //递归
-            return calcTempResult(calcCache);
-        }
-    }
-
-    /**
-     * 计算最终结果
-     *
-     * @return
-     */
-    private double calcResult(CalcCache calcCache) {
-        //打印每次的信息
-        System.out.println(calcCache.toString());
-        //获取栈大小
-        int size = calcCache.valuesSize();
-        if (size == 1) {
-            return calcCache.removeLastValue();
-        } else if (size == 2) {
-            double v1 = calcCache.removeLastValue();
-            char o = calcCache.removeLastSign();
-            double v2 = calcCache.removeLastValue();
-            return OperationFactory.createOperation(o).calc(v1, v2);
-        } else {
-            double v1 = calcCache.removeLastValue();
-            char o = calcCache.removeLastSign();
-            double v2 = calcCache.removeLastValue();
-            double v = OperationFactory.createOperation(o).calc(v1, v2);
-            calcCache.addLastValue(v);
-            //递归
-            return calcResult(calcCache);
+    private void handlePriority() {
+        if (calcCache.checkPriority(0)) {
+            //如果出现优先级低的符号
+            //计算之前符号优先级高的值
+            calcPriority(calcCache);
+            calcCache.setPriority(0);
         }
     }
 }
