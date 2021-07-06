f2321604f5de27e25988f5e22a5fa3d5828f5957
qingshanpeng <qingshanpeng@126.com>
2021-07-06 14:59:52 +0800

    bio

--- java/src/main/java/com/cyen/practice/io/BIOServerMultiThread.java
+++ java/src/main/java/com/cyen/practice/io/BIOServerMultiThread.java
@@ -11,7 +11,7 @@ import java.util.concurrent.Executors;
  * @date 2021/7/2 17:22
  * @since 标果工厂-小白杏
  */
-public class BIOMultiThreadServer {
+public class BIOServerMultiThread {
 	public static void main(String[] args) {
 		byte[] buffer = new byte[1024];
 		ExecutorService executorService = Executors.newFixedThreadPool(5);
@@ -25,13 +25,16 @@ public class BIOMultiThreadServer {
 					try {
 						System.out.println("接收到客户端连接：" + client.getRemoteSocketAddress());
 
-						//发送数据给客户端
-						client.getOutputStream().write("你好，我是服务端发送的数据...".getBytes());
-
+						System.out.println("等待客户端发送数据");
 						//接受客户端的数据
 						client.getInputStream().read(buffer);
 						String content = new String(buffer);
 						System.out.println("接收到客户端数据:" + content);
+
+						//发送数据给客户端
+						client.getOutputStream().write("你好，我是服务端发送的数据...".getBytes());
+
+						client.close();
 					} catch (IOException e) {
 						e.printStackTrace();
 					}
