ca8ee1bb539e434c1f6d9115d29f9a1988d11d5b
qingshanpeng <qingshanpeng@126.com>
2022-06-20 17:48:31 +0800

    ThreadLocal

---
+++ java/src/main/java/com/cyen/practice/jdk_juc/MyInheritableThreadLocal.java
@@ -0,0 +1,12 @@
+package com.cyen.practice.jdk_juc;
+
+/**
+ * @author qingshanpeng
+ * @date 2022/6/20 17:43
+ * @since 标果工厂
+ */
+public class MyInheritableThreadLocal {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ java/src/main/java/com/cyen/practice/jdk_juc/MyTransmittableThreadLocal.java
@@ -0,0 +1,12 @@
+package com.cyen.practice.jdk_juc;
+
+/**
+ * @author qingshanpeng
+ * @date 2022/6/20 16:46
+ * @since 标果工厂
+ */
+public class MyTransmittableThreadLocal {
+    public static void main(String[] args) {
+
+    }
+}
